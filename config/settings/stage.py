a7ff1643d02a52631a3edfcd21ad389d4797e90d
Kostiantyn <kostiantyn.noha@gmail.com>
2016-09-26 21:10:35 +0200

    WEED-10 - Add stage setting file

---
+++ config/settings/stage.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Stage settings
+
+- Run in Debug mode
+
+- Use console backend for emails
+
+- Add Django Debug Toolbar
+- Add django-extensions as app
+"""
+
+import os
+import socket
+
+from .common import *  # noqa
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='!3ioc^sm0fa9-zg9!v7&^-vz98#j(2f5=_kko%&-&cz91b0522')
+
+# Mail settings
+# ------------------------------------------------------------------------------
+
+EMAIL_PORT = 1025
+
+EMAIL_HOST = 'localhost'
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND',
+                    default='django.core.mail.backends.console.EmailBackend')
+
+# CACHING
+# ------------------------------------------------------------------------------
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
+
+INTERNAL_IPS = ['54.70.213.66', ]
+# tricks to have debug toolbar when developing with docker
+if os.environ.get('USE_DOCKER') == 'yes':
+    ip = socket.gethostbyname(socket.gethostname())
+    INTERNAL_IPS += [ip[:-1] + "1"]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+
+# django-extensions
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ('django_extensions',)
+
+# TESTING
+# ------------------------------------------------------------------------------
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'strainrx-ds-oltp',
+        'USER': 'strains_admin',
+        'PASSWORD': '0180c348-6dc6-42f0-9ff9-43cd02162a02',
+        'HOST': 'strainrx-ds-oltp.cpdsc6jugpam.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+HOST = 'http://ec2-54-70-213-66.us-west-2.compute.amazonaws.com:8000'
