52fd9d923f6dbc36ab5eac0dbde01c15d987e1c1
Kostiantyn Noha <kostiantyn.noha@gmail.com>
2016-11-01 19:43:16 +0100

    WEED-84, WEED-87, WEED-80 - fixes (#78)

--- web/search/admin.py
+++ web/search/admin.py
@@ -6,7 +6,10 @@ from web.search.models import *
 
 @admin.register(Strain)
 class StrainAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'category', 'variety']
+    search_fields = ['name', 'category', 'variety']
+    list_filter = ['name', 'category', 'variety']
+    ordering = ['name']
 
 
 @admin.register(StrainImage)
