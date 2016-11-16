31ea30f78dff1cfb68e301432bd3448298ced58c
Kostiantyn <kostiantyn.noha@gmail.com>
2016-11-16 11:53:59 +0100

    WEED-99 - CR comments

--- web/businesses/api/permissions.py
+++ web/businesses/api/permissions.py
@@ -7,5 +7,4 @@ class BusinessAccountOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         business_id = view.kwargs.get('business_id')
         filterargs = {'users__id': request.user.id, 'pk': business_id}
-        businesses = Business.objects.filter(**filterargs)
-        return len(businesses) > 0
+        return Business.objects.filter(**filterargs).exists()
