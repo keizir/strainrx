8f2f89696c85d983c69623a9ed6bbe734672a781
Kostiantyn <kostiantyn.noha@gmail.com>
2016-11-25 00:23:55 +0100

    WEED-92 - Bug fixes

--- web/search/views.py
+++ web/search/views.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-from web.search.es_service import SearchElasticService
 from web.search.models import Strain
 
 
@@ -14,22 +13,6 @@ class StrainSearchWizardView(LoginRequiredMixin, TemplateView):
 
 class StrainSearchResultView(LoginRequiredMixin, TemplateView):
     template_name = 'pages/search/strain/search_results.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(StrainSearchResultView, self).get_context_data(**kwargs)
-        r = self.request
-
-        search_criteria = r.session.get('search_criteria')
-        start_from = r.GET.get('from')
-
-        if search_criteria:
-            data = SearchElasticService().query_strain_srx_score(search_criteria, 8, start_from)
-            result_list = data.get('list')
-            context['search_results'] = result_list
-            context['search_results_total'] = data.get('total')
-            return context
-
-        return context
 
 
 class StrainDetailView(LoginRequiredMixin, TemplateView):
