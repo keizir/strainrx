98313702fc73f274559cd641c63c3b4079e6c666
Kostiantyn Noha <kostiantyn.noha@gmail.com>
2017-03-29 01:02:34 +0200

    WEED-289 - Adds custom 404 page for SDP (#276)
    * WEED-289 - Adds custom 404 page for SDP
    
    * WEED-289 - Update 404 page to be generic

--- web/search/views.py
+++ web/search/views.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.views.generic import TemplateView
 
 from web.search.models import Strain
@@ -19,12 +20,19 @@ class StrainDetailView(TemplateView):
     template_name = 'pages/strain/strain_detail.html'
 
     def get_context_data(self, **kwargs):
-        slug_name = kwargs.get('slug_name')
-        strain = Strain.objects.get(strain_slug=slug_name)
         context = super(StrainDetailView, self).get_context_data(**kwargs)
-        context['strain_id'] = strain.id
-        context['strain_name'] = strain.name
-        context['strain_variety'] = strain.variety
+
+        slug_name = kwargs.get('slug_name')
+        strain_variety = kwargs.get('strain_variety')
+
+        if Strain.objects.filter(variety=strain_variety, strain_slug=slug_name).exists():
+            strain = Strain.objects.get(variety=strain_variety, strain_slug=slug_name)
+            context['strain_id'] = strain.id
+            context['strain_name'] = strain.name
+            context['strain_variety'] = strain.variety
+        else:
+            raise Http404
+
         return context
 
 
