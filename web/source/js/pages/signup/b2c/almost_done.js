317763ea01d672fd15b658cc26f73c76a4d30276
Piotr Nowakowski <morseusz@gmail.com>
2017-12-22 15:52:03 +0100

    Verification email confirmation

--- web/source/js/pages/signup/b2c/almost_done.js
+++ web/source/js/pages/signup/b2c/almost_done.js
@@ -6,13 +6,27 @@ W.pages.b2c.AlmostDone = function () {
 
     return {
         init: function () {
-            $('.resend-email-link').on('click', function (e) {
+            var resend = $('.resend');
+
+            var callApi = function(e) {
+                resend.html('Sending email ...');
+
                 e.preventDefault();
                 $.ajax({
                     method: 'GET',
                     url: '/api/v1/users/resend-email-confirmation'
+                }).success(function() {
+                    resend.html('Email sent successfully.');
+                }).fail(function() {
+                    resend.html(
+                        "There was a problem with sending your email.\n" +
+                        '<a class="resend-email-link" href="#">Click here</a> to resend it.'
+                    );
+                    $('.resend-email-link').on('click', callApi);
                 });
-            });
+            };
+
+            $('.resend-email-link').on('click', callApi);
         }
     };
 }();
